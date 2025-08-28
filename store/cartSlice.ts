05044783164f85bbb852f93d678befa20070c95d
Mohamed Gamal <mohamed-gamal-20190427>
2025-08-28 04:03:03 +0300

    add redux

---
+++ store/cartSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice,PayloadAction } from "@reduxjs/toolkit";
+
+
+export interface CartItem{
+
+    id:number;
+    title:string;
+    price:number;
+    category:string;
+    image:string;
+    rating:{
+        rate:number;
+        count:number;
+    };
+    quantity:number
+} 
+
+
+
+export interface CartState{
+    items:CartItem[];
+}
+
+
+const initialState:CartState={
+    items:[],
+}
+
+const cartSlice =createSlice({
+    name:"cart",
+    initialState,
+    reducers:{
+        addItem:(state,action:PayloadAction<Omit<CartItem,"quantity">>)=>{
+            const existingItem = state.items.find(
+                (item)=>item.id===action.payload.id
+            );
+        },
+    },
+})
